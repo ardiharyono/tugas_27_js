a3b4d2eeef88d2942d4a97d279cba910d600dae5
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:42:36 +0700

    Tugas 27 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas27.js
@@ -0,0 +1,12 @@
+var a = "No";
+var b = "rumah saya";
+var c = 30;
+var d = "A";
+var e = "Jalan pangeran Tirtayasa No";
+var f = 70;
+
+hasil1 = a + " " + b + " " + c + d;
+console.log(hasil1);
+console.log(hasil1);
+hasil2 = e + " " + f;
+console.log(hasil2);
